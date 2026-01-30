7200e990904c6fcd0d0577b509326908470e120b
Pat <patrycja.kruczynska@gmail.com>
2026-01-30 19:45:59 +0100

    model transaction finished

--- src/main/java/com/example/payment_app/model/Transaction.java
+++ src/main/java/com/example/payment_app/model/Transaction.java
@@ -1,16 +1,28 @@
 package com.example.payment_app.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="transactions")
 public class Transaction {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Integer id;
-    private Integer sender_id;
-    private Integer receiver_id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver", nullable = false)
+    private User receiver;
+
+    @Column(name="description")
     private String description;
+
+    @Column(name="amount")
     private Double amount;
 
 }
