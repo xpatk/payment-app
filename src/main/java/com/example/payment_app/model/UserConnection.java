1099d1e49c51fc0dbdaf69098dbbb0bb2dec6174
Pat <patrycja.kruczynska@gmail.com>
2026-01-31 19:14:51 +0100

    user connection model

--- src/main/java/com/example/payment_app/model/UserConnection.java
+++ src/main/java/com/example/payment_app/model/UserConnection.java
@@ -6,16 +6,40 @@ import jakarta.persistence.*;
 @Table(name = "user_connections")
 public class UserConnection {
 
-    @EmbeddedId
-    private UserConnectionId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
 
     @ManyToOne
-    @MapsId("userId")
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne
-    @MapsId("connectionId")
     @JoinColumn(name = "connection_id", nullable = false)
     private User connection;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getConnection() {
+        return connection;
+    }
+
+    public void setConnection(User connection) {
+        this.connection = connection;
+    }
 }
