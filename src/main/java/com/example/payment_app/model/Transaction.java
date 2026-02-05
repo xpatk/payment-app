895ec7303980bf94fc5b5d39b12168039787f977
Pat <patrycja.kruczynska@gmail.com>
2026-02-05 23:35:33 +0100

    fix model

--- src/main/java/com/example/payment_app/model/Transaction.java
+++ src/main/java/com/example/payment_app/model/Transaction.java
@@ -11,12 +11,12 @@ public class Transaction {
     @Column(name="id")
     private Integer id;
 
-    @ManyToOne
-    @JoinColumn(name = "sender", nullable = false)
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
     private User sender;
 
-    @ManyToOne
-    @JoinColumn(name = "receiver", nullable = false)
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;
 
     @Column(name="description")
@@ -24,5 +24,45 @@ public class Transaction {
 
     @Column(name="amount")
     private Double amount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 
 }
