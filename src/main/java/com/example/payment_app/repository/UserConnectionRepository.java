57315c68189e08c0415d49bb880305a95ae67ab6
Pat <patrycja.kruczynska@gmail.com>
2026-02-05 17:11:04 +0100

    user connections get service

--- src/main/java/com/example/payment_app/repository/UserConnectionRepository.java
+++ src/main/java/com/example/payment_app/repository/UserConnectionRepository.java
@@ -1,9 +1,15 @@
 package com.example.payment_app.repository;
 
+import com.example.payment_app.model.User;
 import com.example.payment_app.model.UserConnection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserConnectionRepository extends JpaRepository<UserConnection, Integer> {
+
+    List<UserConnection> findByUser(User user);
+
 }
--- src/main/java/com/example/payment_app/service/UserConnectionService.java
+++ src/main/java/com/example/payment_app/service/UserConnectionService.java
@@ -1,14 +1,24 @@
 package com.example.payment_app.service;
 
+import com.example.payment_app.model.User;
 import com.example.payment_app.model.UserConnection;
 import com.example.payment_app.repository.UserConnectionRepository;
-import com.example.payment_app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserConnectionService {
 
     @Autowired
     private UserConnectionRepository userConnectionRepository;
+
+    public List<User> getConnectionsForUser(User user) {
+        return userConnectionRepository.findByUser(user)
+                .stream()
+                .map(UserConnection::getConnection)
+                .toList();
+    }
+
 }
