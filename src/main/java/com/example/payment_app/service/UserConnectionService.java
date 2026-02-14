4c922561ffaaf9bf78252109705f309a1b3cf712
Pat <patrycja.kruczynska@gmail.com>
2026-02-14 20:03:34 +0100

    Filled in missing functions

--- src/main/java/com/example/payment_app/service/UserConnectionService.java
+++ src/main/java/com/example/payment_app/service/UserConnectionService.java
@@ -3,6 +3,7 @@ package com.example.payment_app.service;
 import com.example.payment_app.model.User;
 import com.example.payment_app.model.UserConnection;
 import com.example.payment_app.repository.UserConnectionRepository;
+import com.example.payment_app.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,9 @@ public class UserConnectionService {
     @Autowired
     private UserConnectionRepository userConnectionRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public List<User> getConnectionsForUser(User user) {
         return userConnectionRepository.findByUser(user)
                 .stream()
@@ -24,10 +28,53 @@ public class UserConnectionService {
 
     @Transactional
     public User saveConnection(User user, String connectionEmail) {
-        //check if connection exists
-        //add connection
+
+        User connection = userRepository.findByEmail(connectionEmail);
+
+        if(connection == null) {
+            throw new RuntimeException("User not found");
+        }
+
+        if(user.getUserId().equals(connection.getUserId())) {
+            throw new IllegalArgumentException("You cannot add your own account as connection");
+        }
+
+        boolean alreadyExists = userConnectionRepository
+                .findByUser(user)
+                .stream()
+                .anyMatch(uc -> uc.getConnection().getUserId().equals(connection.getUserId()));
+
+        if (alreadyExists) {
+            throw new RuntimeException("This connection already exists.");
+        }
+
+        UserConnection userConnection = new UserConnection();
+        userConnection.setUser(user);
+        userConnection.setConnection(connection);
+
+        userConnectionRepository.save(userConnection);
+
+        return connection;
+
     }
 
-    // delete connection
+    @Transactional
+    public void deleteConnection (User user, String connectionEmail) {
+
+        User connection = userRepository.findByEmail(connectionEmail);
+
+        if (connection == null) {
+            throw new RuntimeException("This connection doesn't exist");
+        }
+
+        UserConnection userConnection = userConnectionRepository
+                .findByUser(user)
+                .stream()
+                .filter(uc -> uc.getConnection().getUserId().equals(connection.getUserId()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Connection not found."));
+
+        userConnectionRepository.delete(userConnection);
+    }
 
 }
